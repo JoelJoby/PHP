1d0c49f1b917469c6fdf895690684e7df7fdef77
Joel Joby <joeljoby28430@gmail.com>
2023-08-01 23:12:15 +0530

    first commit

---
+++ Basics/printing.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>This is myfirst php page</h1>
+
+    <?php
+
+    $color = "red";
+    $x = 100;
+    echo "hello world" . "<br>";
+    Echo "Hello World" . "<br>";
+    echo "my house is " . $color . "<br>";
+
+    //echo $color + $x;
+    
+    ?>
+
+</body>
+</html>
