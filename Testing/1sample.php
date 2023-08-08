0a0c37cf442fefa0dd1f393c121a7818a71b50c0
Joel Joby <joeljoby28430@gmail.com>
2023-08-08 17:32:42 +0530

    simple feedback forms

---
+++ Testing/1sample.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    welocom <?php echo $_GET["name"]; ?> <br>
+    Your Entered email is <?php echo $_GET["email"];?><br>
+    Your number is <?php echo $_GET["phone"]; ?> <br>
+</body>
+</html>
